80790d81ff5270c149b2ca24e33f0c67cd9e7ea0
raymondcastro-hub <raymond.castro@student.northampton.edu>
2026-07-11 16:47:33 -0400

    Update dependencies and replace V2 references with template throughout the repository

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, scam-proof-your-life-v2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, scam-proof-your-life-template');
   });
 });
